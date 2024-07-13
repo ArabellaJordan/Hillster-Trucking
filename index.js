cb84c7d5eceb2d75af63f01c07b022c5fa9c5cd3
Arabella <jordanfaearabella@gmail.com>
2024-07-14 07:24:34 +0800

    working css

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.use(express.static(__dirname));
+
+
 app.get("/", (req, res) => {
-  // res.send("eme");
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}.`);
+  console.log(__dirname);
 });
