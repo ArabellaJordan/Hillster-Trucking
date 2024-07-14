1c5b42c3fb564abb0cd2a9f9f34e210a876bdbdb
Arabella <jordanfaearabella@gmail.com>
2024-07-14 08:58:08 +0800

    login

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended:true }));
 
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.post("/verify-login", (req, res) => {
+  // console.log(req.body);
+  res.sendFile(__dirname + "/public/Tracking.html");
 });
 
 app.listen(port, () => {
